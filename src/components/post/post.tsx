9608a10f5ba8ef14b16b0e6de3ca88c3d8f0e064
MamatovY <mamatovyiman28@gmail.com>
2024-03-21 12:16:51 +0600

    Added comments

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,39 +1,42 @@
-import { useParams } from 'react-router-dom'
-import styles from './post.module.scss'
-import { useEffect, useState } from 'react'
-import { Post } from 'types'
-
-
-
+import { useParams } from 'react-router-dom';
+import styles from './post.module.scss';
+import { useEffect, useState } from 'react';
+import { Post } from 'types';
 
 const ThePost = () => {
-    const [post, setPost] = useState<Post | null>(null)
-    const { id } = useParams()
+    // Состояние для хранения данных о посте
+    const [post, setPost] = useState<Post | null>(null);
+    // Получаем параметр id из URL
+    const { id } = useParams();
 
-    const fetchPosts = async (id: string,) => {
+    // Функция для загрузки данных о посте по его id
+    const fetchPosts = async (id: string) => {
         try {
             const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
             const data: Post = await response.json();
 
+            // Обновляем состояние с данными о посте
             setPost(data);
-
         } catch (error) {
             console.error('Ошибка при получении поста:', error);
         }
     };
 
+    // Загружаем данные о посте при монтировании компонента и при изменении id в URL
     useEffect(() => {
-        if (id)
+        if (id) {
             fetchPosts(id);
+        }
     }, [id]);
 
+    // Отображаем данные о посте, если они загружены
     return post && (
         <div className={styles.post}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
             <p>{post.id}</p>
         </div>
-    )
+    );
 }
 
-export default ThePost
+export default ThePost;
--- src/components/posts/posts.tsx
+++ src/components/posts/posts.tsx
@@ -4,67 +4,88 @@ import styles from './posts.module.scss';
 import { Post } from 'types';
 
 
-
 const ThePosts: React.FC = () => {
+  // Получаем объекты навигации и местоположения из react-router-dom
   const navigate = useNavigate();
   const location = useLocation();
+  // Получаем параметры из URL
   const searchParams = new URLSearchParams(location.search);
+  // Получаем номер страницы из параметров или устанавливаем значение по умолчанию (1)
   let initialPage = parseInt(searchParams.get('page') || '1', 10);
-  const initialLimit = initialPage * 10
+  // Вычисляем начальное значение лимита на основе номера страницы (для первой страницы устанавливаем 10, для последующих - page * 10)
+  const initialLimit = initialPage === 1 ? 10 : initialPage * 10;
+
+  // Список постов
   const [posts, setPosts] = useState<Post[]>([]);
+  // Флаг загрузки данных
   const [loading, setLoading] = useState<boolean>(true);
+  // Номер текущей страницы
   const [page, setPage] = useState<number>(1);
+  // Лимит на запрос к бекенду
   const [limit, setLimit] = useState<number>(initialLimit);
+  // Флаг чтобы узнать есть ли еше данные
   const [hasMore, setHasMore] = useState<boolean>(true);
+  // Счетчик автоматически загруженных страниц
   const [loadedPages, setLoadedPages] = useState<number>(0);
 
+  // Запрос постов при загрузке компонента и при изменении номера страницы
   useEffect(() => {
-    fetchPosts(page, limit)
-  }, [page])
+    fetchPosts(page, limit);
+  }, [page]);
 
-
+  // Функция для запроса постов с сервера
   const fetchPosts = async (pageParam: number, limit: number) => {
     try {
       const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=${limit}`);
       const data: Post[] = await response.json();
+      // Обновляем состояния
       setPosts([...posts, ...data]);
-      setLoading(false);
-      setLimit(10)
 
+      // Устанавливаем флаг загрузки данных в значение false после успешной загрузки
+      setLoading(false);
+
+      // Устанавливаем лимит на количество элементов на странице в 10 после каждой загрузки
+      setLimit(10);
+
+      // Проверяем, есть ли еще данные для загрузки
+      // Если длина полученного массива данных равна 0, значит, больше нет данных для загрузки
       if (data.length === 0) {
+        // Устанавливаем флаг hasMore в значение false, чтобы скрыть кнопку "Load More"
         setHasMore(false);
       }
+
     } catch (error) {
       console.error('Ошибка при получении постов:', error);
     }
   };
 
+  // Обработчик скролла страницы для автоматической подгрузки постов
   const handleScroll = () => {
+    // Проверяем, доскроллен ли пользователь до конца страницы, и нет ли загрузки в процессе
     if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) return;
-    if (!hasMore) return;
-    if (loadedPages < 5) {
-      setPage(initialPage + 1);
-
-      const newSearchParams = new URLSearchParams(location.search);
-      newSearchParams.set('page', String(initialPage + 1));
-      navigate(`?${newSearchParams.toString()}`, { replace: true });
-
-      setLoadedPages(prevLoadedPages => prevLoadedPages + 1);
-    }
+    // Проверяем, есть ли еще данные для загрузки и не превышено ли ограничение по числу загруженных страниц (5 раз)
+    if (!hasMore || loadedPages >= 5) return;
+    // Увеличиваем номер страницы, обновляем URL с новыми параметрами и обновляем счетчик загруженных страниц
+    setPage(initialPage + 1);
+    const newSearchParams = new URLSearchParams(location.search);
+    newSearchParams.set('page', String(initialPage + 1));
+    navigate(`?${newSearchParams.toString()}`, { replace: true });
+    setLoadedPages(prevLoadedPages => prevLoadedPages + 1);
   };
 
+  // Добавляем обработчик скролла при монтировании компонента и убираем его при размонтировании
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, [loading, hasMore, loadedPages]);
 
+  // Функция для загрузки следующей порции постов по кнопке "Load More"
   const loadMore = () => {
     setPage(prevPage => prevPage + 1);
     const newSearchParams = new URLSearchParams(location.search);
     newSearchParams.set('page', String(page + 1));
     navigate(`?${newSearchParams.toString()}`, { replace: true });
   };
-
   return (
     <>
       <div className={styles.posts}>
@@ -76,10 +97,12 @@ const ThePosts: React.FC = () => {
           </Link>
         ))}
       </div>
+      {/* Отображаем загрузчик, если данные загружаются */}
       {loading && <p>Loading...</p>}
+      {/* Отображаем кнопку "Load More" только если есть еще данные для загрузки и не превышено ограничение по числу автоматически загруженных страниц (5 раз) */}
       {!loading && hasMore && loadedPages >= 5 && <button onClick={loadMore}>Load More</button>}
     </>
   );
 };
 
-export default ThePosts
+export default ThePosts;
